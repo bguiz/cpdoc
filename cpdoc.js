89ce34f40a23055ce3f3110b7c6994491c45478d
bguiz <bguiz@users.noreply.github.com>
2020-06-15 14:31:03 +0800

    feat: skeletal structure for iteration

--- cpdoc.js
+++ cpdoc.js
@@ -1,5 +1,39 @@
-function cpdoc() {
-  console.log('cpdoc');
+const path = require('path');
+
+async function cpdoc(config, configFileName) {
+  console.log(configFileName);
+  const processGroupsPromises = config.groups.map((group) => (processGroup(group, config)));
+  return Promise.all(processGroupsPromises);
+}
+
+async function processGroup(group, config) {
+  const {
+    id,
+    rootDir,
+    copyMethod,
+    filters,
+    postProcessors,
+    files,
+  } = group;
+
+  const processFilesPromises = files.map((file) => (processFile(file, group, config)));
+  return Promise.all(processFilesPromises);
+}
+
+async function processFile(file, group, config) {
+  const {
+    remote,
+    local,
+  } = file;
+  const {
+    id: groupId,
+    rootDir,
+  } = group;
+  const {
+    id: configId,
+  } = config;
+  const localPath = path.join(rootDir, local);
+  console.log('processFile', configId, groupId, remote, localPath);
 }
 
 module.exports = {
