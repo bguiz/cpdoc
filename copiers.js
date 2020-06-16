38571a83522ad0ca84f0284cc1a76dc91b74ebcf
bguiz <bguiz@users.noreply.github.com>
2020-06-16 13:32:56 +0800

    feat: add new copier to save from URL directly to file

--- copiers.js
+++ copiers.js
@@ -1,17 +1,23 @@
+const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
+function getHttpOrHttps(url) {
+  const protocol = url.split('://')[0];
+  let httpOrHttps;
+  if (protocol === 'http') {
+    httpOrHttps = http;
+  } else if (protocol === 'https') {
+    httpOrHttps = https;
+  } else {
+    return reject(new Error(`Unsupported protocol ${protocol}`));
+  }
+  return httpOrHttps;
+}
+
 function httpGet(url) {
   return new Promise((resolve, reject) => {
-    const protocol = url.split('://')[0];
-    let httpOrHttps;
-    if (protocol === 'http') {
-      httpOrHttps = http;
-    } else if (protocol === 'https') {
-      httpOrHttps = https;
-    } else {
-      return reject(new Error(`Unsupported protocol ${protocol}`));
-    }
+    httpOrHttps = getHttpOrHttps(url);
 
     httpOrHttps.get(url, (res) => {
       const { statusCode } = res;
@@ -39,6 +45,28 @@ function httpGet(url) {
   });
 }
 
+function httpGetSaveFile(url, fileName) {
+  return new Promise((resolve, reject) => {
+    httpOrHttps = getHttpOrHttps(url);
+    const file = fs.createWriteStream(fileName);
+    httpOrHttps.get(url, (response) => {
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close((err) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      });
+    }).on('error', (err) => {
+      fs.unlink(fileName);
+      reject(err);
+    });
+  });
+}
+
 module.exports = {
   httpGet,
+  httpGetSaveFile,
 };
