011216ef8c9b78c7290d62a4233eabaa0e8f944b
bguiz <bguiz@users.noreply.github.com>
2020-06-16 13:32:00 +0800

    feat: add util function to parse filename from URL, and sanitise string for use as an ID

--- utils.js
+++ utils.js
@@ -1,13 +1,37 @@
 const fs = require('fs');
 const util = require('util');
-const http = require('http');
-const https = require('https');
 
 const fsWriteFile = util.promisify(fs.writeFile);
 
 const fsReadFile = util.promisify(fs.readFile);
 
+function sanitiseAsId(str) {
+  return str.toLowerCase().replace(/[^#\-\w\d]/g, '');
+}
+
+function parseUrlFileName(url) {
+  const posLastDot = url.lastIndexOf('.');
+  const posLastSlash = url.lastIndexOf('/');
+  const path = url.substring(0, posLastSlash);
+  let fileName;
+  let fileExt;
+  if (posLastDot < posLastSlash) {
+    fileName = url.substring(posLastSlash + 1);
+    fileExt = '';
+  } else {
+    fileName = url.substring(posLastSlash + 1, posLastDot);
+    fileExt = url.substring(posLastDot + 1);
+  }
+  return {
+    path,
+    fileName,
+    fileExt,
+  };
+}
+
 module.exports = {
   fsWriteFile,
   fsReadFile,
+  sanitiseAsId,
+  parseUrlFileName,
 };
